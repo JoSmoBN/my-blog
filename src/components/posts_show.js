aa408e882234013f9216b9cbf2a6348c03a74893
Joseph Nolan <josephn0624@gmail.com>
2017-06-02 17:27:56 -0500

    started adding show single post route

---
+++ src/components/posts_show.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ShowPost extends Component {
+    render() {
+        return (
+            <div>
+                Posts Show!
+            </div>
+        )
+    }
+}
+
+export default ShowPost
